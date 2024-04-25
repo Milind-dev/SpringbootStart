9f9cb8581f6694c79d568c4e6aaed1753a855a4a
Milind-dev <milinddev1011@gmail.com>
2024-04-25 13:03:08 +0530

    GlobalExceptionHandler ResponseEntityHandler

--- src/main/java/com/milinddev/expensetrackerapi/entity/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/milinddev/expensetrackerapi/entity/exceptions/GlobalExceptionHandler.java
@@ -1,18 +1,26 @@
 package com.milinddev.expensetrackerapi.entity.exceptions;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import java.util.stream.Collectors;
 
 import com.milinddev.expensetrackerapi.entity.ErrorObject;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler{
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<ErrorObject> handleExpenseNotFoundException(ResourceNotFoundException ex, WebRequest request){
 		ErrorObject errorobject = new ErrorObject();
@@ -37,4 +45,23 @@ public class GlobalExceptionHandler {
 			errorobject.setTimestamp(new Date());
 		  return new ResponseEntity<ErrorObject>(errorobject,HttpStatus.INTERNAL_SERVER_ERROR);
 	   }
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+			Map<String, Object> body = new HashMap<String, Object>();
+			body.put("timestamp", new Date());
+	//		body.put("statusCode", HttpStatus.BAD_REQUEST);
+			body.put("statusCode", HttpStatus.BAD_REQUEST.value());
+			List<String> errors = ex.getBindingResult()
+					.getFieldErrors()
+					.stream()
+					.map(x -> x.getDefaultMessage())
+					.collect(Collectors.toList());
+				
+			body.put("messages", errors);
+			
+			return new ResponseEntity<Object>(body, HttpStatus.BAD_REQUEST);
+			
+	}
 }
