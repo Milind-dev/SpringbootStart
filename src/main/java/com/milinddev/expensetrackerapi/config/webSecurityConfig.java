d57d7c1f46b638ad8b80aad4b0ffb43889b03515
Milind Dev <56596354+Milind-dev@users.noreply.github.com>
2024-04-27 22:46:20 +0530

    websecurityConfig and pom.xml

--- src/main/java/com/milinddev/expensetrackerapi/config/webSecurityConfig.java
+++ src/main/java/com/milinddev/expensetrackerapi/config/webSecurityConfig.java
@@ -1,35 +1,72 @@
 package com.milinddev.expensetrackerapi.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.authentication.jaas.memory.InMemoryConfiguration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import com.milinddev.expensetrackerapi.security.CustomUserDetailsService;
 
 @Configuration
-public class webSecurityConfig extends webSecurityConfigureAdapter{ //webSecurityConfigureAdapter are depressiated in 3.5
+@EnableWebSecurity
+public class webSecurityConfig {
 	
 	
 	private CustomUserDetailsService userDetailsService;
-	private AuthenticationManager authenticationManager;
+//	private AuthenticationManager authenticationManager;
 	
-	@Overide
-	protected void configure(Httpsecurity http) throws Exception{
-		http
-		   .csrf().disable()
-		   .autorizeRequest()
-		   .anyMatchers("/login","/register").permitAll()
-		   .anyRequest().authenticate()
-		   .and()
-		   .httpBasics();
-	}
+//	@Overide
+//	protected void configure(Httpsecurity http) throws Exception{
+//		http
+//		   .csrf().disable()
+//		   .autorizeRequest()
+//		   .anyMatchers("/login","/register").permitAll()
+//		   .anyRequest().authenticate()
+//		   .and()
+//		   .httpBasics();
+//	}
+	
+
+	
+//	@Bean
+//	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//	    http
+//	        .csrf().disable()
+//	        .authorizeRequests()
+//	            .requestMatchers("/login", "/register").permitAll()
+//	            .anyRequest().authenticated()
+//	        .and()
+//	        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//	        .and()
+//	        .httpBasic();
+//	    return http.build();
+//	}
+//	@Bean
+//	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+//		http
+//		.csrf().disable()
+//		.authorizeHttpRequests()
+//		.requestMatchers("/login", "/register").permitAll()
+//		.anyRequest().authenticated()
+//		.and()
+//		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+////		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+//		http.httpBasic();
+//		return http.build();
+//	}
 //	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 //		InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
 //		UserDetails user1 = User.withUsername("milind").password("12345").authorities("admin").build();
@@ -46,19 +83,32 @@ public class webSecurityConfig extends webSecurityConfigureAdapter{ //webSecurit
 //	}
 //	
 
-	public void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService);
-		
-	}
+//	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		auth.userDetailsService(userDetailsService);
+//	}
 
 	@Bean
 	public PasswordEncoder passwordEndEncoder() {
 //		return NoOpPasswordEncoder.getInstance();
 		return new BCryptPasswordEncoder();	
 	}
-	@Bean
-	public AuthenticationManager authenticationManagerBean() throws Exception {
 	
-		return super.authenticationManagerBean();
+//	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		
+		authProvider.setUserDetailsService(userDetailsService);
+		authProvider.setPasswordEncoder(passwordEndEncoder());
+		
+		return authProvider;
 	}
+	
+//	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+		return authConfig.getAuthenticationManager();
+	}
+//	@Bean
+//	public AuthenticationManager authenticationManagerBean() throws Exception {
+//		return super.authenticationManagerBean();
+//	}
 }
