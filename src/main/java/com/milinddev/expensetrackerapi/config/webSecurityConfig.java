690a589cc04757d6b9114d95ce3babe84f294966
Milind-dev <milinddev1011@gmail.com>
2024-04-27 17:08:31 +0530

    websecurity depressiated and i just practice, here passwordEncoder and inMemoryUserDetailsManager

--- src/main/java/com/milinddev/expensetrackerapi/config/webSecurityConfig.java
+++ src/main/java/com/milinddev/expensetrackerapi/config/webSecurityConfig.java
@@ -1,7 +1,13 @@
 package com.milinddev.expensetrackerapi.config;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.jaas.memory.InMemoryConfiguration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class webSecurityConfig extends webSecurityConfigureAdapter{ //webSecurityConfigureAdapter are depressiated in 3.5
@@ -17,11 +23,21 @@ public class webSecurityConfig extends webSecurityConfigureAdapter{ //webSecurit
 		   .httpBasics();
 	}
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-		.withUser("milind").password("12345").authorities("admin")
-		.and()
-		.withUser("pant").password("12345").authorities("user")
-		.and()
-		.passwordEncoder(NoOpPasswordEncoder.getInstance());
+		InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+		UserDetails user1 = User.withUsername("milind").password("12345").authorities("admin").build();
+		UserDetails user2 = User.withUsername("pant").password("12345").authorities("user").build();
+		userDetailsService.createUser(user1);
+		userDetailsService.createUser(user2);
+		auth.userDetailsService(userDetailsService);
+//		auth.inMemoryAuthentication()
+//		.withUser("milind").password("12345").authorities("admin")
+//		.and()
+//		.withUser("pant").password("12345").authorities("user")
+//		.and()
+//		.passwordEncoder(NoOpPasswordEncoder.getInstance());
+	}
+	@Bean
+	public PasswordEncoder passwordEndEncoder() {
+		return NoOpPasswordEncoder.getInstance();
 	}
 }
