b7772a81dc58c0d72d4e32491f8ee6ca2197b59f
Milind-dev <milinddev1011@gmail.com>
2024-04-27 17:39:27 +0530

    webSecurityConfig and customUserDetailsService

--- src/main/java/com/milinddev/expensetrackerapi/config/webSecurityConfig.java
+++ src/main/java/com/milinddev/expensetrackerapi/config/webSecurityConfig.java
@@ -9,8 +9,13 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import com.milinddev.expensetrackerapi.security.CustomUserDetailsService;
+
 @Configuration
 public class webSecurityConfig extends webSecurityConfigureAdapter{ //webSecurityConfigureAdapter are depressiated in 3.5
+	
+	
+	private CustomUserDetailsService userDetailsService;
 	
 	@Overide
 	protected void configure(Httpsecurity http) throws Exception{
@@ -22,20 +27,26 @@ public class webSecurityConfig extends webSecurityConfigureAdapter{ //webSecurit
 		   .and()
 		   .httpBasics();
 	}
+//	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+//		UserDetails user1 = User.withUsername("milind").password("12345").authorities("admin").build();
+//		UserDetails user2 = User.withUsername("pant").password("12345").authorities("user").build();
+//		userDetailsService.createUser(user1);
+//		userDetailsService.createUser(user2);
+//		auth.userDetailsService(userDetailsService);
+////		auth.inMemoryAuthentication()
+////		.withUser("milind").password("12345").authorities("admin")
+////		.and()
+////		.withUser("pant").password("12345").authorities("user")
+////		.and()
+////		.passwordEncoder(NoOpPasswordEncoder.getInstance());
+//	}
+//	
+
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
-		InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
-		UserDetails user1 = User.withUsername("milind").password("12345").authorities("admin").build();
-		UserDetails user2 = User.withUsername("pant").password("12345").authorities("user").build();
-		userDetailsService.createUser(user1);
-		userDetailsService.createUser(user2);
 		auth.userDetailsService(userDetailsService);
-//		auth.inMemoryAuthentication()
-//		.withUser("milind").password("12345").authorities("admin")
-//		.and()
-//		.withUser("pant").password("12345").authorities("user")
-//		.and()
-//		.passwordEncoder(NoOpPasswordEncoder.getInstance());
 	}
+
 	@Bean
 	public PasswordEncoder passwordEndEncoder() {
 		return NoOpPasswordEncoder.getInstance();
